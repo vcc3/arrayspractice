e1ca23ccaa662b593bda359ef169c2db5a0a4879
vcc3 <vcc3@njit.edu>
2016-10-12 23:49:07 -0400

    add values

---
+++ arrayvalues.php
@@ -0,0 +1,7 @@
+<?php
+
+$array = array("size" => "XL", "color" => "gold");
+//0 is assigned to size, 1 is assigned to gold.  the first value in quotes is just the value.
+print_r(array_values($array));
+
+?>
